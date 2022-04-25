037f103c04603e1b51140e2baceb67445017f013
Mariusz Stróż <manieczny86@gmail.com>
2022-04-25 22:50:57 +0200

    Testing AXIOS

--- nuxt/components/Tutorial.vue
+++ nuxt/components/Tutorial.vue
@@ -1,11 +1,35 @@
 <!-- Please remove this file from your project -->
 <template>
   <div>
-    <b-container class="mt-2">
+    <b-container class="mt-5">
       <b-row>
         <b-col>
-          <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
-          <b-button variant="success" @click="send">Send</b-button>
+          <b-jumbotron>
+
+            <h1>NUXT + YII2 application on DOCKER</h1>
+            <p>This application is simple example of setup Nuxt as fronted and YII 2 as backed.</p>
+
+            <b-form @submit.prevent="send">
+              <b-form-group
+                id="input-group-1"
+                label="Email address:"
+                label-for="input-1"
+                description="We'll never share your email with anyone else."
+              >
+                <b-form-input
+                  id="input-1"
+                  v-model="form.email"
+                  type="text"
+                  placeholder="Enter email"
+                ></b-form-input>
+              </b-form-group>
+
+
+              <b-button type="submit" variant="primary">Submit</b-button>
+            </b-form>
+
+          </b-jumbotron>
+
         </b-col>
       </b-row>
     </b-container>
@@ -17,13 +41,14 @@ export default {
   name: 'NuxtTutorial',
   data() {
     return {
-      text: ''
+      form: {
+        email: ''
+      }
     }
   },
   methods: {
-    async send() {
-
-      await this.$axios.$get('http://localhost:8000/crawler')
+    send() {
+      this.$axios.$get('/crawler')
     }
   }
 }
--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -9,12 +9,12 @@ export default {
       lang: 'en',
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''},
+      {name: 'format-detection', content: 'telephone=no'},
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -43,7 +43,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: '/yii',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
